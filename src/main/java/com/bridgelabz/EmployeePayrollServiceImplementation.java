5be88f760acc047b6457fd41fc7547f0f294b1ed
Pooja <pooja.14206@gmail.com>
2021-05-21 09:28:21 +0530

    added a file to perform operations in salary

--- src/main/java/com/bridgelabz/EmployeePayrollServiceImplementation.java
+++ src/main/java/com/bridgelabz/EmployeePayrollServiceImplementation.java
@@ -2,6 +2,7 @@ package com.bridgelabz;
 
 import java.sql.*;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class EmployeePayrollServiceImplementation implements EmployeePayrollService {
@@ -20,7 +21,7 @@ public class EmployeePayrollServiceImplementation implements EmployeePayrollServ
     public List<EmployeePayrollData> getEmployees() throws EmployeePayrollException, SQLException {
         String sql = "select * from employeepayroll;";
         List<EmployeePayrollData> employeePayrollData = new ArrayList<EmployeePayrollData>();
-        try  {
+        try {
             Connection connection = this.getConnection();
             Statement statement = connection.createStatement();
             ResultSet resultSet = statement.executeQuery(sql);
@@ -33,21 +34,23 @@ public class EmployeePayrollServiceImplementation implements EmployeePayrollServ
                 employeePayrollData.add(new EmployeePayrollData(id, name, salary, start, gender));
             }
         } catch (SQLException e) {
-            throw new EmployeePayrollException("Cannot establish connection",EmployeePayrollException.ExceptionType.CONNECTION_FAIL);
+            throw new EmployeePayrollException("Cannot establish connection", EmployeePayrollException.ExceptionType.CONNECTION_FAIL);
         }
         return employeePayrollData;
     }
+
     @Override
     public int getEmployeePayrollData(String Name, int salary) throws EmployeePayrollException {
-            String sql = String.format("update  employeepayroll set salary=%d where name='%s'", salary, Name);
-            try {
-                Connection connection = this.getConnection();
-                Statement statement = connection.createStatement();
-                return statement.executeUpdate(sql);
-            } catch (SQLException throwables) {
-                throw new EmployeePayrollException("Cannot establish connection",EmployeePayrollException.ExceptionType.CONNECTION_FAIL);
-            }
+        String sql = String.format("update  employeepayroll set salary=%d where name='%s'", salary, Name);
+        try {
+            Connection connection = this.getConnection();
+            Statement statement = connection.createStatement();
+            return statement.executeUpdate(sql);
+        } catch (SQLException throwables) {
+            throw new EmployeePayrollException("Cannot establish connection", EmployeePayrollException.ExceptionType.CONNECTION_FAIL);
+        }
     }
+
     @Override
     public int preparedStatmentForWholeTableData(String Name, int salary) throws EmployeePayrollException {
         String sql = String.format("update  employeepayroll set salary=10 where name='Roni'");
@@ -56,14 +59,15 @@ public class EmployeePayrollServiceImplementation implements EmployeePayrollServ
             Statement statement = connection.prepareStatement(sql);
             return statement.executeUpdate(sql);
         } catch (SQLException throwables) {
-            throw new EmployeePayrollException("Cannot establish connection",EmployeePayrollException.ExceptionType.CONNECTION_FAIL);
+            throw new EmployeePayrollException("Cannot establish connection", EmployeePayrollException.ExceptionType.CONNECTION_FAIL);
         }
     }
+
     @Override
     public List<EmployeePayrollData> preparedStatmentForWholeTableData(String name) throws EmployeePayrollException {
         String sql = "select * from employeepayroll where name = 'Kitti'";
         List<EmployeePayrollData> employeePayrollData = new ArrayList<EmployeePayrollData>();
-        try  {
+        try {
             Connection connection = this.getConnection();
             Statement statement = connection.createStatement();
             ResultSet resultSet = statement.executeQuery(sql);
@@ -76,10 +80,11 @@ public class EmployeePayrollServiceImplementation implements EmployeePayrollServ
                 employeePayrollData.add(new EmployeePayrollData(id, name, salary, start, gender));
             }
         } catch (SQLException e) {
-            throw new EmployeePayrollException("Cannot establish connection",EmployeePayrollException.ExceptionType.CONNECTION_FAIL);
+            throw new EmployeePayrollException("Cannot establish connection", EmployeePayrollException.ExceptionType.CONNECTION_FAIL);
         }
         return employeePayrollData;
     }
+
     public ArrayList<EmployeePayrollData> findEmployesJoinesForDateRange(String dateStart, String dateEnd) throws EmployeePayrollException {
         String sql = String.format("select * from employeepayroll where start between cast('%s' as date) and cast('%s' as date)", dateStart, dateEnd);
         List<EmployeePayrollData> employeePayrollData = new ArrayList<>();
@@ -96,10 +101,33 @@ public class EmployeePayrollServiceImplementation implements EmployeePayrollServ
                 employeePayrollData.add(new EmployeePayrollData(id, name, salary, start, gender));
             }
         } catch (SQLException e) {
-            throw new EmployeePayrollException("Cannot establish connection",EmployeePayrollException.ExceptionType.CONNECTION_FAIL);
+            throw new EmployeePayrollException("Cannot establish connection", EmployeePayrollException.ExceptionType.CONNECTION_FAIL);
         }
         return (ArrayList<EmployeePayrollData>) employeePayrollData;
     }
 
+    public HashMap<String, Integer> performOperationsOnSalaryOf(String operation) {
+        HashMap<String, Integer> resultList = new HashMap<>();
+        String sql = String.format("select gender,%s(salary) from employeepayroll group by gender", operation);
+        try {
+            Connection connection = this.getConnection();
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(sql);
+            while (resultSet.next()) {
+                String gender = resultSet.getString("gender");
+                String coloumnName = operation + "(salary)";
+                int salary = resultSet.getInt(coloumnName);
+                resultList.put(gender, salary);
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return resultList;
+    }
+    public int performOperationOnSalaryOfEmployees(String operation) {
+        EmployeePayrollServiceImplementation employeePayrollServiceImplementation = new EmployeePayrollServiceImplementation();
+        HashMap<String, Integer> resultMap = employeePayrollServiceImplementation.performOperationsOnSalaryOf(operation);
+        int result = resultMap.get("F");
+        return result;
+    }
 }
-
--- src/test/java/com/bridgelabz/EmployeePayrollServiceTest.java
+++ src/test/java/com/bridgelabz/EmployeePayrollServiceTest.java
@@ -4,7 +4,9 @@ import org.junit.Assert;
 import org.junit.Test;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class EmployeePayrollServiceTest {
     @Test
@@ -12,33 +14,50 @@ public class EmployeePayrollServiceTest {
         EmployeePayrollServiceImplementation employeePayrollServiceImplementation = new EmployeePayrollServiceImplementation();
         List<EmployeePayrollData> employeePayrollData = employeePayrollServiceImplementation.getEmployees();
         System.out.println(employeePayrollData);
-        Assert.assertEquals(7,employeePayrollData.size());
+        Assert.assertEquals(7, employeePayrollData.size());
     }
 
     @Test
     public void givenNewSalaryForEmployee_WhenUpdated_ShouldMatch() throws EmployeePayrollException {
         EmployeePayrollServiceImplementation employeePayrollServiceImplementation = new EmployeePayrollServiceImplementation();
-        int employeePayrollData = employeePayrollServiceImplementation.getEmployeePayrollData("Roni",32000);
-        Assert.assertEquals(1,employeePayrollData);
+        int employeePayrollData = employeePayrollServiceImplementation.getEmployeePayrollData("Roni", 32000);
+        Assert.assertEquals(1, employeePayrollData);
     }
+
     @Test
     public void givenNewSalaryForEmployee_WhenUpdated_ShouldMatchUsingPreparedStatement() throws EmployeePayrollException {
         EmployeePayrollServiceImplementation employeePayrollServiceImplementation = new EmployeePayrollServiceImplementation();
-        int employeePayrollData = employeePayrollServiceImplementation.preparedStatmentForWholeTableData("Roni",10);
-        Assert.assertEquals(1,employeePayrollData);
+        int employeePayrollData = employeePayrollServiceImplementation.preparedStatmentForWholeTableData("Roni", 10);
+        Assert.assertEquals(1, employeePayrollData);
     }
+
     @Test
     public void givenNameForEmployee_WhenRetrieved_ShouldMatchEmployeeCount() throws EmployeePayrollException {
         EmployeePayrollServiceImplementation employeePayrollServiceImplementation = new EmployeePayrollServiceImplementation();
         List<EmployeePayrollData> employeePayrollData = employeePayrollServiceImplementation.preparedStatmentForWholeTableData("Kitti");
         System.out.println(employeePayrollData);
-        Assert.assertEquals(1,employeePayrollData.size());
+        Assert.assertEquals(1, employeePayrollData.size());
     }
+
     @Test
     public void givenDataRangeWeShouldBeAbleToFindAllTheEmployessJoined() throws EmployeePayrollException {
         EmployeePayrollServiceImplementation employeePayrollServiceImplementation = new EmployeePayrollServiceImplementation();
         ArrayList<EmployeePayrollData> employesJoinesForDateRange = employeePayrollServiceImplementation.findEmployesJoinesForDateRange("2018-11-09", "2020-01-10");
         System.out.println(employesJoinesForDateRange);
         Assert.assertEquals(3, employesJoinesForDateRange.size());
+    }
+
+    @Test
+    public void givenEmployeePayrollInDB_WhenRetrievedForSum_ShouldMatchedSumSalaryForGender() throws EmployeePayrollException {
+        EmployeePayrollServiceImplementation employeePayrollServiceImplementation = new EmployeePayrollServiceImplementation();
+        Assert.assertEquals(410000, employeePayrollServiceImplementation.performOperationOnSalaryOfEmployees("sum"));
+        Assert.assertEquals(68333, employeePayrollServiceImplementation.performOperationOnSalaryOfEmployees("avg"), 0.0);
+        Assert.assertEquals(90000, employeePayrollServiceImplementation.performOperationOnSalaryOfEmployees("max"), 0.0);
+        Assert.assertEquals(30000, employeePayrollServiceImplementation.performOperationOnSalaryOfEmployees("min"), 0.0);
+        System.out.println("Sum: " + employeePayrollServiceImplementation.performOperationOnSalaryOfEmployees("sum"));
+        System.out.println("Avg: " + employeePayrollServiceImplementation.performOperationOnSalaryOfEmployees("avg"));
+        System.out.println("Max: " + employeePayrollServiceImplementation.performOperationOnSalaryOfEmployees("max"));
+        System.out.println("Min: " + employeePayrollServiceImplementation.performOperationOnSalaryOfEmployees("min"));
+
     }
 }
